31840150f3996920b63c3d58b69db419a220b669
Ema Fresco <efresco@udesa.edu.ar>
2025-10-27 14:48:11 -0300

    termine la practica, agregando el login y los comentarios

--- Clase10/practica1/src/Screens/Home.js
+++ Clase10/practica1/src/Screens/Home.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Comentarios from '../Components/DinamicForms';
 
-class Home extends Component {
+export default class Home extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
           <Text style={styles.title}>Home</Text>
+          <Comentarios/>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,               
+    backgroundColor: '#f5f6fa',
+    alignItems: 'center',  
+    justifyContent: 'flex-start', 
+    padding: 20,
+  },
   title: {
     fontSize: 22,
     fontWeight: 'bold',
     color: '#2f3640',
-},
+    marginBottom: 20,
+  },
 });
-export default Home
